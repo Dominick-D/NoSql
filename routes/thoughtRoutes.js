caafb433b25741885e0522ccdb6a0bf672c220c9
Dominick-D <127568682+Dominick-D@users.noreply.github.com>
2023-08-07 17:23:50 -0400

    8/7 5:23PM
    Fixed file structure,
    Added some of the code,
    ect

---
+++ routes/thoughtRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const {
+  // Import methods from controller
+} = require('../../controllers/thought-controller');
+
+// Set up routes e.g., GET, POST, PUT, DELETE
+
+module.exports = router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const {
+  // Import methods from controller
+} = require('../../controllers/user-controller');
+
+// Set up routes e.g., GET, POST, PUT, DELETE
+
+module.exports = router;
